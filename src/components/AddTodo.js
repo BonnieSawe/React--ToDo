51f5e29f89ef4c063408d80cc48dc56201fa1534
Boniface Sawe <kiplangatbonifaces@gmail.com>
2019-12-17 16:49:25 +0300

    update

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 export class AddTodo extends Component {
   state = {
     title: ''
@@ -17,20 +16,20 @@ export class AddTodo extends Component {
   render() {
     return (
       <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
-        <input 
-          type="text" 
-          name="title" 
-          style={{ flex: '10', padding: '5px' }}
-          placeholder="Add Todo ..." 
-          value={this.state.title}
-          onChange={this.onChange}
-        />
-        <input 
-          type="submit" 
-          value="Submit" 
-          className="btn"
-          style={{flex: '1'}}
-        />
+          <input 
+            type="text" 
+            name="title" 
+            className="form-control todo-list-input"
+            // style={{ flex: '10', padding: '5px' }}
+            placeholder="Add Todo ..." 
+            value={this.state.title}
+            onChange={this.onChange}
+          />
+          <input 
+            type="submit" 
+            value="Add" 
+            className="add btn btn-primary font-weight-bold todo-list-add-btn"
+          />
       </form>
     )
   }
